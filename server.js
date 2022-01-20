9397e6bf7496e3330862fa8c58eec54d643f366d
Blake Georgii <blakegeorgii@gmail.com>
2022-01-19 22:13:51 -0800

    added front end files

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
