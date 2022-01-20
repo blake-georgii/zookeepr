5e653060e3f89178ed9b3ad9995e5f8bc8b23165
Blake Georgii <blakegeorgii@gmail.com>
2022-01-20 13:09:34 -0800

    connected html pages to seperate paths

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
